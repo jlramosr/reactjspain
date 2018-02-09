4324a54f92adaf990f3e0d58a4b4d5aea2ae0a98
Jose Ramos <jlramosr@gmail.com>
2018-02-09 19:18:59 +0100

    header

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div className="App-title">
+            <h1>const message = 'Welcome to ReactJSpain'</h1>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
